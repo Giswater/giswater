f6757b61f6855ba059ef1281ccc641db749a38d2
Vicente Medina <vicente.medina@gits.ws>
2015-02-25 03:10:12 +0100

    Updated functions in "update/hecras" and in "sql". Defining "search_path" is required before calling the functions.
    Signed-off-by: Vicente Medina <vicente.medina@gits.ws>

--- updates/hecras/hecras_11101.sql
+++ updates/hecras/hecras_11101.sql
@@ -9,72 +9,74 @@ This version of Giswater is provided by Giswater Association
 -- Changes incorporated in version 1.1.101 - 09/09/2014 
 --------------------------------------------------------------------------------------------------
 
+
+SET search_path = "SCHEMA_NAME", pg_catalog;
+
+
 --
 -- TOC entry 1687 (class 1255 OID 30312)
 -- Name: gr_export_geo(character varying, boolean, boolean, boolean, boolean, boolean); Type: FUNCTION; Schema: SCHEMA_NAME; Owner: postgres
 --
-DROP FUNCTION "SCHEMA_NAME"."gr_export_geo"();
-DROP FUNCTION "SCHEMA_NAME"."gr_export_geo"(filename varchar);
 
-CREATE FUNCTION "SCHEMA_NAME"."gr_export_geo"("filename" character varying, "SA" boolean, "IA" boolean, "Levees" boolean, "BO" boolean, "Manning" boolean) RETURNS integer
+CREATE OR REPLACE FUNCTION "SCHEMA_NAME".gr_export_geo("filename" character varying, "SA" boolean, "IA" boolean, "Levees" boolean, "BO" boolean, "Manning" boolean) RETURNS integer
     LANGUAGE "plpgsql"
-    AS 'DECLARE
+    AS $$DECLARE
 
 	numRows integer;
 
 BEGIN
 
 	--Clear log
-	PERFORM "SCHEMA_NAME".gr_clear_log();
+	PERFORM gr_clear_log();
 
 	--Log
-	INSERT INTO "SCHEMA_NAME".log VALUES (''gr_export_geo()'', ''Empty log'', CURRENT_TIMESTAMP);
+	INSERT INTO log VALUES ('gr_export_geo()', 'Empty log', CURRENT_TIMESTAMP);
 
 	--Clear error
-	PERFORM "SCHEMA_NAME".gr_clear_error();
+	PERFORM gr_clear_error();
 
 	--Compute stream length
-	PERFORM "SCHEMA_NAME".gr_stream_length();
+	PERFORM gr_stream_length();
 
 	--XS station
-	PERFORM "SCHEMA_NAME".gr_xs_station();
+	PERFORM gr_xs_station();
 
 	--XS banks
-	PERFORM "SCHEMA_NAME".gr_xs_banks();
+	PERFORM gr_xs_banks();
 
 	--XS flowpaths
-	PERFORM "SCHEMA_NAME".gr_xs_lengths();
+	PERFORM gr_xs_lengths();
 
 	--Check errors
-	SELECT INTO numRows count(*) FROM "SCHEMA_NAME".error;
+	SELECT INTO numRows count(*) FROM error;
 
 	IF numRows > 0 THEN
-		INSERT INTO "SCHEMA_NAME".log VALUES (''gr_export_geo()'', ''Sdf export error!'', CURRENT_TIMESTAMP);
+		INSERT INTO log VALUES ('gr_export_geo()', 'Sdf export error!', CURRENT_TIMESTAMP);
 		RETURN -10;
 	END IF;
 
 	--Dump river & XS data to outfile and export to sdf
-	PERFORM "SCHEMA_NAME".gr_dump_sdf(filename);
+	PERFORM gr_dump_sdf(filename);
 
 	--Check errors
-	SELECT INTO numRows count(*) FROM "SCHEMA_NAME".error;
+	SELECT INTO numRows count(*) FROM error;
 
 	IF numRows > 0 THEN
-		INSERT INTO "SCHEMA_NAME".log VALUES (''gr_export_geo()'', ''Sdf export error!'', CURRENT_TIMESTAMP);
+		INSERT INTO log VALUES ('gr_export_geo()', 'Sdf export error!', CURRENT_TIMESTAMP);
 		RETURN -20;
 	END IF;
 
 	--Update 3d layers
-	PERFORM "SCHEMA_NAME".gr_fill_3d_tables();
+	PERFORM gr_fill_3d_tables();
 
 	--Log
-	INSERT INTO "SCHEMA_NAME".log VALUES (''gr_export_geo()'', ''Sdf file finished'', CURRENT_TIMESTAMP);
+	INSERT INTO log VALUES ('gr_export_geo()', 'Sdf file finished', CURRENT_TIMESTAMP);
 
 	--Return
 	RETURN 0;
 
 END;
-';
+$$;
 
 
 -- ------------------------------------------------------------
