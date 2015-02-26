129fa1b167453583d482ae066a6317b63aff6918
Vicente Medina <vicente.medina@gits.ws>
2015-02-26 22:00:55 +0100

    Update "clone_schema" function in HEC-RAS to include the functions contained in the schema.
    Signed-off-by: Vicente Medina <vicente.medina@gits.ws>

--- updates/hecras/hecras_11101.sql
+++ updates/hecras/hecras_11101.sql
@@ -90,10 +90,16 @@ DECLARE
 	rec_view record;
 	rec_fk record;
 	rec_table text;
+	rec_function record;
+	rec_parameters record;
 	tablename text;
 	default_ text;
 	column_ text;
 	msg text;
+	parameters_text text;
+
+	on_delete_text text;
+	on_update_text text;
 BEGIN
 
 	-- Create destination schema
@@ -103,7 +109,7 @@ BEGIN
 	FOR rec_table IN
 		SELECT sequence_name FROM information_schema.SEQUENCES WHERE sequence_schema = source_schema
 	LOOP
-		EXECUTE 'CREATE SEQUENCE ' || dest_schema || '.' || rec_table;
+		EXECUTE 'CREATE SEQUENCE ' || dest_schema || '.' || quote_ident(rec_table);
 	END LOOP;
 	 
 	-- Tables
@@ -112,20 +118,20 @@ BEGIN
 	LOOP
 	  
 	  	-- Create table in destination schema
-		tablename := dest_schema || '.' || rec_table;
-		EXECUTE 'CREATE TABLE ' || tablename || ' (LIKE ' || source_schema || '.' || rec_table || ' INCLUDING CONSTRAINTS INCLUDING INDEXES INCLUDING DEFAULTS)';
+		tablename := dest_schema || '.' || quote_ident(rec_table);
+		EXECUTE 'CREATE TABLE ' || tablename || ' (LIKE ' || source_schema || '.' || quote_ident(rec_table) || ' INCLUDING CONSTRAINTS INCLUDING INDEXES INCLUDING DEFAULTS)';
 		
 		-- Set contraints
 		FOR column_, default_ IN
 			SELECT column_name, REPLACE(column_default, source_schema, dest_schema) 
 			FROM information_schema.COLUMNS 
-			WHERE table_schema = dest_schema AND table_name = rec_table AND column_default LIKE 'nextval(%' || source_schema || '%::regclass)'
+			WHERE table_schema = dest_schema AND table_name = quote_ident(rec_table) AND column_default LIKE 'nextval(%' || source_schema || '%::regclass)'
 		LOOP
 			EXECUTE 'ALTER TABLE ' || tablename || ' ALTER COLUMN ' || column_ || ' SET DEFAULT ' || default_;
 		END LOOP;
 		
 		-- Copy table contents to destination schema
-		EXECUTE 'INSERT INTO ' || tablename || ' SELECT * FROM ' || source_schema || '.' || rec_table; 	
+		EXECUTE 'INSERT INTO ' || tablename || ' SELECT * FROM ' || source_schema || '.' || quote_ident(rec_table); 	
 		
 	END LOOP;
 	  
@@ -134,7 +140,7 @@ BEGIN
 		SELECT table_name FROM information_schema.TABLES WHERE table_schema = source_schema AND table_type = 'BASE TABLE' ORDER BY table_name
 	LOOP	  
 	  
-		tablename := dest_schema || '.' || rec_table;	  
+		tablename := dest_schema || '.' || quote_ident(rec_table);	  
 		FOR rec_fk IN
 			SELECT tc.constraint_name, tc.constraint_schema, tc.table_name, kcu.column_name,
 			ccu.table_name AS parent_table, ccu.column_name AS parent_column,
@@ -152,10 +158,25 @@ BEGIN
 				ON rc.unique_constraint_catalog = ccu.constraint_catalog
 				AND rc.unique_constraint_schema = ccu.constraint_schema
 				AND rc.unique_constraint_name = ccu.constraint_name
-			WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.constraint_schema = source_schema AND tc.table_name = rec_table
+			WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.constraint_schema = source_schema AND tc.table_name = quote_ident(rec_table)
 		LOOP
-			msg:= 'ALTER TABLE '||tablename||' ADD CONSTRAINT '||rec_fk.constraint_name||' FOREIGN KEY('||rec_fk.column_name||') 
-				REFERENCES '||dest_schema||'.'||rec_fk.parent_table||'('||rec_fk.parent_column||') ON DELETE '||rec_fk.on_delete||' ON UPDATE '||rec_fk.on_update;
+
+--			Check action
+			IF (rec_fk.on_delete = 'NO ACTION') THEN
+				on_delete_text := 'NO ACTION';
+			ELSE
+				on_delete_text := quote_ident(rec_fk.on_delete);
+			END IF;
+			
+			IF (rec_fk.on_update = 'NO ACTION') THEN
+				on_update_text := 'NO ACTION';
+			ELSE
+				on_update_text := quote_ident(rec_fk.on_update);
+			END IF;
+						
+			msg:= 'ALTER TABLE '||tablename||' ADD CONSTRAINT '||quote_ident(rec_fk.constraint_name)||' FOREIGN KEY('||quote_ident(rec_fk.column_name)||') 
+				REFERENCES '||dest_schema||'.'||quote_ident(rec_fk.parent_table)||'('||quote_ident(rec_fk.parent_column)||') ON DELETE '||on_delete_text||' ON UPDATE '||on_update_text;
+
 			EXECUTE msg;
 		END LOOP;		
 		
@@ -165,9 +186,37 @@ BEGIN
 	FOR rec_view IN
 		SELECT table_name, REPLACE(view_definition, source_schema, dest_schema) as definition FROM information_schema.VIEWS WHERE table_schema = source_schema
 	LOOP
-		EXECUTE 'CREATE VIEW ' || dest_schema || '.' || rec_view.table_name || ' AS ' || rec_view.definition;
+		EXECUTE 'CREATE VIEW ' || dest_schema || '.' || quote_ident(rec_view.table_name) || ' AS ' || rec_view.definition;
 	END LOOP;
- 
+
+	-- Functions
+	SET check_function_bodies = false;
+	
+	FOR rec_function IN
+		SELECT routine_name, REPLACE(routine_definition, source_schema, dest_schema) as definition, type_udt_name, external_language, specific_name FROM information_schema.ROUTINES WHERE routine_schema = source_schema
+	LOOP
+
+		-- Get function parameters
+		parameters_text := '';
+
+		FOR rec_parameters IN
+			SELECT parameter_mode, parameter_name, udt_name FROM information_schema.PARAMETERS WHERE specific_name = rec_function.specific_name ORDER BY ordinal_position
+		LOOP
+			parameters_text := parameters_text || rec_parameters.parameter_mode || ' ' || rec_parameters.parameter_name || ' ' || rec_parameters.udt_name || ', ';
+		END LOOP;
+
+		IF (char_length(parameters_text) > 0) THEN
+			parameters_text := substring(parameters_text from 1 for (char_length(parameters_text) - 2));
+		END IF;
+
+		IF (rec_function.type_udt_name != 'trigger') THEN
+			msg := 'CREATE FUNCTION ' || dest_schema || '.' || quote_ident(rec_function.routine_name) || '(' ||parameters_text|| ') RETURNS ' || rec_function.type_udt_name || ' AS $BODY' || '$' || rec_function.definition || '$BODY' || '$'
+			|| ' LANGUAGE '|| rec_function.external_language ||' VOLATILE COST 100;';
+			EXECUTE msg;
+		END IF;
+
+	END LOOP;	
+
 END;
 $$;
 
