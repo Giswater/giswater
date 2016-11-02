ecb6641735e85ebb4f8a42d45c95cda51d1df712
David Erill <derill@bgeo.es>
2016-11-02 17:05:09 +0100

    Bug fix

--- src/org/giswater/task/DbToFilesTask.java
+++ src/org/giswater/task/DbToFilesTask.java
@@ -197,6 +197,8 @@ public class DbToFilesTask extends ParentSchemaTask {
 		content = "DROP VIEW IF EXISTS \""+this.currentSchemaName+"\".\""+rsView.getString("viewname")+"\";";
 		content+= "\nCREATE VIEW \""+this.currentSchemaName+"\".\""+rsView.getString("viewname")+"\" AS";
 		content+= "\n"+rsView.getString("definition")+"\n\n";
+		content = content.replace("\""+this.currentSchemaName+"\"", "\"SCHEMA_NAME\"");
+		content = content.replace("\t", "    ");		
 		
 		String filePath = folderPath+File.separator+rsView.getString("viewname")+".sql";
 		Utils.logInfo("Generating file: "+filePath);
