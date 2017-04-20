47b7f63e3f3cedf4bd305f16484a2fb23629aaa4
Xavier Torret <xtorret@bgeo.es>
2017-04-20 09:34:16 +0200

    #issue-34 WiP

--- sql/updates/utils/is_last.sql
+++ sql/updates/utils/is_last.sql
@@ -5,6 +5,7 @@ This version of Giswater is provided by Giswater Association
 */
 
 
+
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 
@@ -17,4 +18,31 @@ arc_id2 character varying(16),
 node_id character varying(16),
 tstamp timestamp(6) without time zone,
 "user" character varying(50)
+  CONSTRAINT om_traceability_pkey PRIMARY KEY (id)
 );
+
+
+
+CREATE TABLE config_client_dvalue(
+  id serial NOT NULL,
+  table_id text,
+  column_id text,
+  dv_table text,
+  dv_key_column text,
+  dv_value_column text,
+  orderby_value boolean,
+  allow_null boolean,
+  CONSTRAINT config_client_dvalue_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE mollet_ide_ud.config_client_dvalue ADD CONSTRAINT config_client_value_origin_id_fkey FOREIGN KEY (table_id,column_id) 
+REFERENCES mollet_ide_ud.db_cat_table_x_column (db_cat_table_id,column_name) MATCH SIMPLE ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE mollet_ide_ud.config_client_dvalue ADD CONSTRAINT config_client_value_colunm_id_fkey FOREIGN KEY (dv_table,dv_key_column) 
+REFERENCES mollet_ide_ud.db_cat_table_x_column (db_cat_table_id,column_name) MATCH SIMPLE ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE mollet_ide_ud.db_cat_table_x_column ADD CONSTRAINT db_cat_table_x_column_db_cat_table_id_column_name_key UNIQUE(db_cat_table_id, column_name);
+
+
+-- TO DO:
+-- cal fer una funció que fa un 'update' de les taules de catàleg per tenir-ho sempre actualitzat
