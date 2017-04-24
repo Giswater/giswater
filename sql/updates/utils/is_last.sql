16ccc0d6e5da3dac76a88fd5657ee0fe56788a7d
bbrzepka <bbrzepka@bgeo.es>
2017-04-24 17:32:43 +0200

    issue-34 update on is last

--- sql/updates/utils/is_last.sql
+++ sql/updates/utils/is_last.sql
@@ -8,8 +8,9 @@ This version of Giswater is provided by Giswater Association
 SET search_path = "SCHEMA_NAME", public, pg_catalog;
 
 
+
 CREATE TABLE om_traceability (
-id serial,
+id serial NOT NULL PRIMARY KEY,
 type character varying(50),
 arc_id character varying(16),
 arc_id1 character varying(16),
@@ -18,3 +19,77 @@ node_id character varying(16),
 tstamp timestamp(6) without time zone,
 "user" character varying(50)
 );
+
+
+CREATE TABLE review_arc
+(  arc_id character varying(16),
+  the_geom geometry(MULTILINESTRING,SRID_VALUE),
+  y1 numeric(12,3),
+  y2 numeric(12,3),
+  arc_type character varying(16),
+  arccat_id character varying(30),
+  annotation character varying(254),
+  verified character varying(16),
+  field_checked boolean,
+  office_checked boolean,
+  CONSTRAINT review_arc_pkey PRIMARY KEY (arc_id)
+);
+
+
+CREATE TABLE review_node
+( node_id character varying(16),
+  the_geom geometry(MULTIPOINT,SRID_VALUE),
+  top_elev numeric(12,3),
+  ymax numeric(12,3),
+  node_type character varying(16),
+  cat_matcat character varying(16),
+  dimensions character varying(16),
+  annotation character varying(254),
+  observ character varying(254),
+  verified character varying(16),
+  field_checked boolean,
+  office_checked boolean,
+  CONSTRAINT review_node_pkey PRIMARY KEY (node_id)
+  );
+  
+
+CREATE TABLE review_audit_arc
+(  arc_id character varying(16) NOT NULL,
+  the_geom geometry(MULTILINESTRING,SRID_VALUE),
+  y1 numeric(12,3),
+  y2 numeric(12,3),
+  arc_type character varying(16),
+  arccat_id character varying(30),
+  annotation character varying(254),
+  verified character varying(16),
+   moved_geom boolean,
+  field_checked boolean,
+  "operation" character varying(25),
+  "user" varchar (50),  
+  date_field timestamp (6) without time zone,
+  office_checked boolean,
+  CONSTRAINT review_audit_arc_pkey PRIMARY KEY (arc_id)
+  );
+  
+
+CREATE TABLE review_audit_node
+(  node_id character varying(16),
+  the_geom geometry(MULTIPOINT,SRID_VALUE),
+  top_elev numeric(12,3),
+  ymax numeric(12,3),
+  node_type character varying(16),
+  cat_matcat character varying(16),
+  dimensions character varying(16),
+  annotation character varying(254),
+  observ character varying(254),
+  verified character varying(16),
+  moved_geom boolean,
+  field_checked boolean,
+  "operation" character varying(25),
+  "user" varchar (50),  
+  date_field timestamp (6) without time zone,
+  office_checked boolean,
+  CONSTRAINT review_audit_node_pkey PRIMARY KEY (node_id)
+  );
+  
+  
