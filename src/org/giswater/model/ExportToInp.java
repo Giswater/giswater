7dece15b184b4e8f6d895866157722234d8ca231
David Erill <daviderill79@gmail.com>
2018-01-04 18:18:37 +0100

    Remove unused code

--- src/org/giswater/model/ExportToInp.java
+++ src/org/giswater/model/ExportToInp.java
@@ -74,17 +74,6 @@ public class ExportToInp extends Model {
 		
 	}    
     
-
-    // Execute SQL function gw_fct_node2arc()
-    private static void executeNode2Arc() {
-
-    	Utils.getLogger().info("Execution function 'gw_fct_node2arc()'");
-		String sql = "SELECT "+MainDao.getSchema()+".gw_fct_node2arc();";
-		String result = MainDao.queryToString(sql);		
-		Utils.getLogger().info("Result function 'gw_fct_node2arc()': "+result);
-
-	}
-    
     
     // Execute SQL function gw_fct_pg2epa()
     private static void executePg2Epa(String resultName) {
